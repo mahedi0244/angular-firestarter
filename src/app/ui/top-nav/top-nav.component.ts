e118bc2fc47c44b0925ba47a5e473cec141f868d
mahedi0244 <mahedi0244@gmail.com>
2018-02-05 23:13:30 -0500

    initial commit

--- src/app/ui/top-nav/top-nav.component.ts
+++ src/app/ui/top-nav/top-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/auth.service';
 
 @Component({
   selector: 'top-nav',
@@ -7,10 +8,15 @@ import { Component } from '@angular/core';
 })
 export class TopNavComponent {
 
+  constructor(public auth: AuthService) { }
   show = false;
 
   toggleCollapse() {
     this.show = !this.show;
+  }
+  
+  logout() {
+    this.auth.signOut();
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: 'APIKEY',
-        authDomain: 'DEV-APP.firebaseapp.com',
-        databaseURL: 'https://DEV-APP.firebaseio.com',
-        projectId: 'DEV-APP',
-        storageBucket: 'DEV-APP.appspot.com',
-        messagingSenderId: '123456789'
+        apiKey: "AIzaSyBgsxRU633l6FqjKGHOvRVlzMyapZGsz5I",
+        authDomain: "icnef-cd1e6.firebaseapp.com",
+        databaseURL: "https://icnef-cd1e6.firebaseio.com",
+        projectId: "icnef-cd1e6",
+        storageBucket: "icnef-cd1e6.appspot.com",
+        messagingSenderId: "62578961560"
     }
 };
