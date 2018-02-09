130ea7612274ff5fb1c61f7bf7b6ec611f477367
mahedi0244 <mahedi0244@gmail.com>
2018-02-08 23:26:40 -0500

    added prod config and firebase config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,1 +1,12 @@
 
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBgsxRU633l6FqjKGHOvRVlzMyapZGsz5I",
+        authDomain: "icnef-cd1e6.firebaseapp.com",
+        databaseURL: "https://icnef-cd1e6.firebaseio.com",
+        projectId: "icnef-cd1e6",
+        storageBucket: "icnef-cd1e6.appspot.com",
+        messagingSenderId: "62578961560"
+    }
+};
